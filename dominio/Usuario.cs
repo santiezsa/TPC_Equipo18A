5df2cffb6bd5f095f10692fc2a31fa92fbfa2788
Lautaro Lopez <lautarolopez1498@gmail.com>
2025-10-26 20:58:46 -0300

    Agrego clase Usuario

---
+++ dominio/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }    
+
+        public Perfil Perfil { get; set; }
+
+        public string Password { get; set; }    
+
+        public bool Activo { get; set; } = true;
+    }
+}
