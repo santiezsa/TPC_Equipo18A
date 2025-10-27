258801bbcca39d35276c7941121a5967fc7d017f
Lautaro Lopez <lautarolopez1498@gmail.com>
2025-10-26 21:04:11 -0300

    Agrego enum Perfil

---
+++ dominio/Perfil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public enum Perfil
+    {
+        Administrador = 1,
+        Vendedor = 2,
+    }
+}
