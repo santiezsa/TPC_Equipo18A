98c46057eb00dd6208f820c91dc9dab54784079d
Seremma <serg.emma@hotmail.com>
2025-10-27 21:03:22 -0300

    Agrego Producto

--- dominio/Producto.cs
+++ dominio/Producto.cs
@@ -8,5 +8,15 @@ namespace dominio
 {
     internal class Producto
     {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        //public Marca Marca { get; set; }
+        //public Categoria Categoria { get; set; }
+        //public Proveedor Provedor { get; set; }
+        public decimal Precio { get; set; }
+        public int stock { get; set; }
+
     }
 }
