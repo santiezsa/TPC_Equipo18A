7fabaf4c8fd8c0257df8428fb52d204b1ba020b3
Seremma <serg.emma@hotmail.com>
2025-10-28 22:00:49 -0300

    Datos en Proveedor y funcion ToString en Categoría y Marca

--- dominio/Proveedor.cs
+++ dominio/Proveedor.cs
@@ -9,8 +9,10 @@ namespace dominio
     public class Proveedor
     {
         public int Id { get; set; }
+        public string Nombre { get; set; }
         public string RazonSocial { get; set; }
         public string CUIT { get; set; }
         public string Email { get; set; }
+        public string Telefono { get; set; }
     }
 }
