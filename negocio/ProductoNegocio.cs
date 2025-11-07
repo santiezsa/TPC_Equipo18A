9dd4999e48d7a9b2ba218859c5031ae5560338ea
Seremma <serg.emma@hotmail.com>
2025-11-06 21:24:06 -0300

    Funciones nuevo, modificar, eliminar y filtrar producto

--- negocio/ProductoNegocio.cs
+++ negocio/ProductoNegocio.cs
@@ -86,5 +86,108 @@ namespace negocio
                 datos.cerrarConexion();
             }
         }
+
+        public void agregar(Producto nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("INSERT INTO Productos (Nombre, Descripcion, IdMarca, IdCategoria) VALUES (@Nombre, @Descripcion, @IdMarca, @IdCategoria)");
+                datos.setearParametro("@Nombre", nuevo.Nombre);
+                datos.setearParametro("@Descripcion", nuevo.Descripcion);
+                datos.setearParametro("@IdMarca", nuevo.Marca.Id);
+                datos.setearParametro("@IdCategoria", nuevo.Categoria.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificar(Producto producto)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("UPDATE Productos SET Nombre = @Nombre, Descripcion = @Descripcion, IdMarca = @IdMarca, IdCategoria = @IdCategoria WHERE Id = @Id");
+                datos.setearParametro("@Nombre", producto.Nombre);
+                datos.setearParametro("@Descripcion", producto.Descripcion);
+                datos.setearParametro("@IdMarca", producto.Marca.Id);
+                datos.setearParametro("@IdCategoria", producto.Categoria.Id);
+                datos.setearParametro("@Id", producto.Id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void eliminar(int id)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("DELETE FROM Productos WHERE Id = @Id");
+                datos.setearParametro("@Id", id);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public List<Producto> buscarPorNombre(string nombre)
+        {
+            List<Producto> lista = new List<Producto>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT P.Id, P.Nombre, P.Descripcion, P.IdMarca, M.Descripcion AS MarcaDescripcion, P.IdCategoria, C.Descripcion AS CategoriaDescripcion\r\nFROM Productos AS P\r\nJOIN Marcas AS M ON P.IDMarca = M.Id\r\nJOIN Categorias AS C ON P.IdCategoria = C.Id\r\nWHERE P.Nombre LIKE @Nombre");
+                datos.setearParametro("@Nombre", "%" + nombre + "%");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Producto aux = new Producto();
+                    //Datos basicos
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    // Datos de marca
+                    aux.Marca = new Marca();
+                    aux.Marca.Id = (int)datos.Lector["IdMarca"];
+                    aux.Marca.Descripcion = (string)datos.Lector["MarcaDescripcion"];
+                    // Datos de categoria
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)datos.Lector["IdCategoria"];
+                    aux.Categoria.Descripcion = (string)datos.Lector["CategoriaDescripcion"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
     }
 }
